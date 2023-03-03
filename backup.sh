69b88c027d6534183357cb5bab18f3e731ac6211
oyako-li <oyakoli3@gmail.com>
2023-03-03 12:15:39 +0900

    delete delete_one_month_old

--- backup.sh
+++ backup.sh
@@ -55,29 +55,10 @@ upload_to_bucket() {
     s3 cp - "s3://$S3_BUCKET_NAME/$S3_BUCKET_DIRECTORY/$(date +%Y/%m/%d/backup-%H-%M-%S.sql.gz)"
 }
 
-delete_one_month_old() {
-    # if the zipped backup file is larger than 50 GB add the --expected-size option
-    # see https://docs.aws.amazon.com/cli/latest/reference/s3/cp.html
-    # 現在の日付を取得
-    now=$(date +%s)
-
-    # 1ヶ月前の日付を取得
-    one_month_ago=$(date -d "1 month ago" +%s)
-
-    # ファイル名を作成
-    dilname="$(date -d "@$one_month_ago" +%Y/%m/%d)"
-
-    echo "1ヶ月前のファイル: $dilname"
-    s3 rm "s3://$S3_BUCKET_NAME/$S3_BUCKET_DIRECTORY/$dilname" --recursive
-}
-
 main() {
     ensure_bucket_exists
     echo "Taking backup and uploading it to S3..."
     pg_dump_database | gzip | upload_to_bucket
-    echo "Done."
-    echo "Delete one month ago backup"
-    delete_one_month_old
     echo "Done."
 }
 
